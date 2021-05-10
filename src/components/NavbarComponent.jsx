c696f17e4772e21762ac2bab196cd5ec38af4e10
Ibrahim Rehman <ibrahimrehman0346@gmail.com>
2021-05-10 20:54:47 +0500

    Created Drawer for Explore Button on Navbar

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,19 +1,110 @@
-import React from "react";
+import React, {useState} from "react";
+import clsx from "clsx";
 import CourseraLogo from "../assets/CourseraLogo.png";
 import {Button, TextField} from "@material-ui/core"
 import SearchIcon from '@material-ui/icons/Search';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import {
+    SwipeableDrawer, 
+    makeStyles, 
+    List, ListItem, 
+    ListItemIcon, 
+    ListItemText, 
+    Divider,
+    ListSubheader
+} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    list: {
+      width: 300,
+    },
+    fullList: {
+      width: 'auto',
+    },
+  });
 
 
 function Navbar(){
+    const classes = useStyles();
+    const [state, setState] = useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+      });
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+          return;
+        }
+    
+        setState({ ...state, [anchor]: open });
+      };
+
+
+    const list = (anchor) => (
+        <div
+          className={clsx(classes.list, {
+            [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+          })}
+          role="presentation"
+          onClick={toggleDrawer(anchor, false)}
+          onKeyDown={toggleDrawer(anchor, false)}
+        >
+          <List>
+                <ListSubheader style={{color: "black", fontSize: "1.2rem"}} disableSticky="true">
+                  <strong>Goals</strong>
+                </ListSubheader>
+            {['Take a Free Course', 'Earn a Degree', 'Earn a Certificate', 'Launch or advance your Career'].map((text, index) => (
+              <ListItem button key={text}>
+                {/* <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon> */}
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+                <ListSubheader  style={{color: "black", fontSize: "1.2rem"}} disableSticky="true">
+                    <strong>Subjects</strong>
+                </ListSubheader>
+            {['Data Science', 'Business', 'Computer Science', 'Information Technology', 'Language Learning', 'Health', 'Personal Development', 'Physical Science and Engineering', 'Social Sciences', 'Arts and Humanities', 'Math and Logic'].map((text, index) => (
+              <ListItem button key={text}>
+                {/* <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon> */}
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+                <ListItem style={{marginTop: "20px"}}>
+                    <Button variant="contained" color="primary" className="navbar-btn">
+                    <strong>Browse all Subjects</strong>
+                    </Button>
+                </ListItem>
+          </List>
+        </div>
+      );
+
     return(
         <nav className="navbar">
             <a href="#">
                 <img src={CourseraLogo} alt="Coursera Logo" className="coursera-logo"/>
             </a>
-            <Button variant="contained" color="primary" className="navbar-btn" endIcon={<ArrowDownwardIcon />}>
+
+
+            <Button variant="contained" color="primary" className="navbar-btn" endIcon={<ArrowDownwardIcon />} onClick={toggleDrawer("left", true)}>
                 <strong>Explore</strong>
             </Button>
+
+            <SwipeableDrawer
+                anchor="left"
+                open={state["left"]}
+                onClose={toggleDrawer("left", false)}
+                onOpen={toggleDrawer("left", true)}
+                >
+                {list("left")}
+            </SwipeableDrawer>
+
+
             <TextField id="outlined-basic" label="What do you want to learn?" variant="outlined" className="search-bar"/>
             <Button variant="contained" color="primary" className="search-bar-btn">
                 <SearchIcon />
