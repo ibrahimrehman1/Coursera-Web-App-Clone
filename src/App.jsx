e65c1baf06131c9fd1405238f5b3ab3484375671
ibrahimrehman1 <ibrahimrehman0346@gmail.com>
2022-09-14 12:32:22 +0500

    router dir.

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,24 @@
 import React from "react";
-import {Header} from "./components/HeaderComponent.jsx";
-import {ArticleOne} from "./components/ArticleOneComponent.jsx";
-import {Route, Switch} from "react-router-dom";
-import {HomeComponent} from "./components/HomeComponent.jsx";
-import {ProfileComponent} from "./components/ProfileComponent.jsx";
-import MyPurchases from "./components/MyPurchases.jsx";
-import Updates from "./components/Updates.jsx";
-import Accomplishments from "./components/Accomplishments.jsx";
+import { Header } from "./components/HeaderComponent.jsx";
+import { ArticleOne } from "./components/ArticleOneComponent.jsx";
+import Router from "./router";
 
-
-function FirstMainComponent({history}){
-  if (localStorage.length){
+export function FirstMainComponent({ history }) {
+  if (localStorage.length) {
     history.push("/home");
   }
-  return(
+  return (
     <React.Fragment>
       <main>
         <Header />
-        <ArticleOne/>
+        <ArticleOne />
       </main>
     </React.Fragment>
-  )
+  );
 }
 
-
 function App() {
-  return (
-    <div>
-      <Switch>
-        <Route component={FirstMainComponent} path="/" exact/>
-        <Route component={HomeComponent} path="/home" exact/>
-        <Route component={ProfileComponent} path="/profile" exact/>
-        <Route component={MyPurchases} path="/purchases" exact/>
-        <Route component={Updates} path="/updates" exact/>
-        <Route component={Accomplishments} path="/accomplishments" exact/>
-      </Switch>
-      
-    </div>
-  );
+  return <Router />;
 }
 
 export default App;
---
+++ src/router/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {Route, Switch} from "react-router-dom";
+import {HomeComponent} from "../components/HomeComponent.jsx";
+import {ProfileComponent} from "../components/ProfileComponent.jsx";
+import MyPurchases from "../components/MyPurchases.jsx";
+import Updates from "../components/Updates.jsx";
+import Accomplishments from "../components/Accomplishments.jsx";
+import { FirstMainComponent } from "../App.jsx";
+
+export default function Router(){
+    return(
+        <>
+        <Switch>
+        <Route component={FirstMainComponent} path="/" exact/>
+        <Route component={HomeComponent} path="/home" exact/>
+        <Route component={ProfileComponent} path="/profile" exact/>
+        <Route component={MyPurchases} path="/purchases" exact/>
+        <Route component={Updates} path="/updates" exact/>
+        <Route component={Accomplishments} path="/accomplishments" exact/>
+      </Switch>
+        
+        </>
+    )
+}
